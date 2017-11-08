4b64df5e4f3041465139b3266a7dbcc5cb8e11db
Drew Finelli <asfinelli@gmail.com>
2017-11-08 12:52:14 -0500

    this is the homework file

---
+++ homework.1.rb
@@ -0,0 +1,6 @@
+# I’m trying to give "Wayne" his proper rap name (Lil Wayne) using string methods found in the Ruby documentation. However I am getting an error when I try to run the following code. Give a brief explanation of why I am getting this error and how it can be resolved.
+
+# " Wayne".prepend (Lil)
+```ruby
+" Wayne".prepend "Lil"
+```
